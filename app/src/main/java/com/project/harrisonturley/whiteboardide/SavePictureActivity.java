eb95f950da54d1ad5f017340fbc12b6658b20e2a
Harrison Turley <harrison.turley.1998@gmail.com>
2019-03-22 20:47:03 -0700

    Add functionality to save screen

--- app/src/main/java/com/project/harrisonturley/whiteboardide/SavePictureActivity.java
+++ app/src/main/java/com/project/harrisonturley/whiteboardide/SavePictureActivity.java
@@ -1,9 +1,15 @@
 package com.project.harrisonturley.whiteboardide;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+
+import java.io.File;
 
 public class SavePictureActivity extends AppCompatActivity {
 
@@ -24,11 +30,26 @@ public class SavePictureActivity extends AppCompatActivity {
         setupSavedImage();
     }
 
+    public void onClickSend(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(getString(R.string.saved_image_path), savedImagePath);
+        startActivity(intent);
+    }
+
+    public void onClickCancel(View v) {
+        Intent intent = new Intent(this, PictureActivity.class);
+        startActivity(intent);
+    }
+
     private void setupSavedImage() {
         if (savedImagePath == null) {
             Log.e("Image Path", "Error getting image path");
         }
 
-
+        File imageFile = new File(savedImagePath);
+        if (imageFile.exists()) {
+            Bitmap imgBitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
+            savedImage.setImageBitmap(imgBitmap);
+        }
     }
 }
